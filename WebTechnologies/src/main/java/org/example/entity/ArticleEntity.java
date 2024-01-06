4d444a83ec8090e47603911ce2f63c5b3d189837
anaoprea21 <100606068+anaoprea21@users.noreply.github.com>
2024-01-06 16:47:37 +0200

    Update ArticleEntity.java

--- WebTechnologies/src/main/java/org/example/entity/ArticleEntity.java
+++ WebTechnologies/src/main/java/org/example/entity/ArticleEntity.java
@@ -1,2 +1,28 @@
-package org.example.entity;public class ArticleEntity {
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.helper.User;
+
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "ARTICLE")
+public class ArticleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String text;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private UserEntity author;
+
 }
