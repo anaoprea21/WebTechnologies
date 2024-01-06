66e9b42e6e3e6a6bd1124850910f3eb6ea736282
anaoprea21 <100606068+anaoprea21@users.noreply.github.com>
2024-01-06 16:47:53 +0200

    Update StoryEntity.java

--- WebTechnologies/src/main/java/org/example/entity/StoryEntity.java
+++ WebTechnologies/src/main/java/org/example/entity/StoryEntity.java
@@ -1,2 +1,21 @@
-package org.example.entity;public class StoryEntity {
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "STORY")
+public class StoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
 }
