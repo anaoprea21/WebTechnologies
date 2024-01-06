acc6f19d34f994d9143b11b0ee8b16e19c9d1828
anaoprea21 <100606068+anaoprea21@users.noreply.github.com>
2024-01-06 16:48:15 +0200

    Update UserEntity.java

--- WebTechnologies/src/main/java/org/example/entity/UserEntity.java
+++ WebTechnologies/src/main/java/org/example/entity/UserEntity.java
@@ -1,2 +1,36 @@
 package org.example.entity;public class UserEntity {
 }
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.helper.UserRole;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CUSTOMER")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    private UserRole role;
+    private String pseudonym;
+
+//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JoinColumn(name = "article_id")
+//    private List<ArticleEntity> articles;
+
+}
