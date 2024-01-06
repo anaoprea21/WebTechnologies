718a0acbf4fe0148a89d72cfffdc7e52289f2ee6
anaoprea21 <100606068+anaoprea21@users.noreply.github.com>
2024-01-06 16:48:32 +0200

    Update User.java

--- WebTechnologies/src/main/java/org/example/helper/User.java
+++ WebTechnologies/src/main/java/org/example/helper/User.java
@@ -1,2 +1,17 @@
-package org.example.helper;public class User {
+package org.example.helper;
+
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+public class User {
+
+
+    private String username;
+    private String email;
+    private String password;
+    private UserRole role;
+    private String pseudonym;
+
+
 }
