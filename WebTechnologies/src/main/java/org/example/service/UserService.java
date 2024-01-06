3a4b0ec13b80abeab13dcea86b40af49b93f1071
anaoprea21 <100606068+anaoprea21@users.noreply.github.com>
2024-01-06 16:49:18 +0200

    Update UserService.java

--- WebTechnologies/src/main/java/org/example/service/UserService.java
+++ WebTechnologies/src/main/java/org/example/service/UserService.java
@@ -1,2 +1,30 @@
-package org.example.service;public class UserService {
+package org.example.service;
+
+import org.example.entity.UserEntity;
+import org.example.helper.User;
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public User create(final User user) {
+
+        UserEntity customer = new UserEntity();
+
+        customer.setEmail(user.getEmail());
+        customer.setPassword(user.getPassword());
+        customer.setUsername(user.getUsername());
+
+        customer.setRole(user.getRole());
+        customer.setPseudonym(user.getPseudonym());
+        repository.save(customer);
+
+        return user;
+    }
+
 }
